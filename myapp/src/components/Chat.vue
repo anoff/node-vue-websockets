99fa39d4761ddaa779192a7f9751820ab7952356
anoff <offenhaeuser@gmail.com>
2018-04-18 00:28:18 +0200

    :star2: feat: Chat component

---
+++ myapp/src/components/Chat.vue
@@ -0,0 +1,44 @@
+<template>
+<div class="page-container">
+    <md-app>
+      <md-app-toolbar class="md-primary">
+        <div class="md-toolbar-row">
+          <span class="md-title">My Chat App</span>
+        </div>
+      </md-app-toolbar>
+      <md-app-content>
+        <md-field :class="messageClass">
+          <label>Messages</label>
+          <md-textarea v-model="textarea" disabled></md-textarea>
+        </md-field>
+        <md-field>
+          <label>Your message</label>
+          <md-input v-model="message"></md-input>
+          <md-button class="md-primary md-raised">Submit</md-button>
+        </md-field>
+      </md-app-content>
+    </md-app>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      textarea: "dummy text\nblup\ndummy text"
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.md-app {
+  height: 800px;
+  border: 1px solid rgba(#000, .12);
+}
+.md-textarea {
+  height: 300px;
+}
+</style>
--- myapp/src/main.js
+++ myapp/src/main.js
@@ -3,7 +3,11 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import VueMaterial from 'vue-material'
+import 'vue-material/dist/vue-material.css'
+import 'vue-material/dist/theme/black-green-light.css'
 
+Vue.use(VueMaterial)
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
